3dacbaacbe5375dfd94caae720a477ea1da3db3c
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2018-05-22 22:52:14 +0530

    Add files via upload

---
+++ multiple.js
@@ -0,0 +1,14 @@
+console.log("using for loop");
+var i;
+for (i=1;i<=10;i++)//variable'i' gets incremented in every iteration
+ {
+    console.log(5*i);//during each iteration,i is multiplied to 5 and console logged simultaneously.
+ }      //so 5 gets multiplied to all numbers from 1 to 10,leading to multiple of 5 from 5 to 50.
+i=1 //once again i is set to 1.
+console.log("using while loop");
+while(i<=10)
+                   //during each iteration,i is multiplied to 5 and console logged simultaneously.
+ {     //so 5 gets multiplied to all numbers from 1 to 10,leading to multiple of 5 from 5 to 50.
+    console.log(5*i);
+    i++; // this is because 'i' gets incremented after every iteration.
+ }
